480a5e27e50125469ab61180fdbc06f9eb3001d5
Yuan Gao <example@email.com>
2019-06-03 09:20:39 -0700

    create class Node

---
+++ 401-code-challenges/Data-Structures/src/main/java/Data/linkedList/LinkedList.java
@@ -0,0 +1,18 @@
+package Data.linkedList;
+
+public class LinkedList {
+    private class Node {
+        int val;
+        Node next;
+        Node(int x) {
+            val = x;
+            next = null;
+        }
+    }
+
+    Node head;
+
+
+
+
+}
