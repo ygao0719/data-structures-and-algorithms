73a48ba4eb964a2f3fdd797ff5ab72a3d3046944
Yuan Gao <example@email.com>
2019-06-09 20:30:31 -0700

    done class stack

--- 401-code-challenges/Data-Structures/src/main/java/stacksandqueues/Stack.java
+++ 401-code-challenges/Data-Structures/src/main/java/stacksandqueues/Stack.java
@@ -1,5 +1,28 @@
 package stacksandqueues;
 
 public class Stack {
+    private Node top;
+
+    public Stack(){
+        top = null;
+    }
+
+    public void push(int input){
+        Node newNode = new Node(input);
+        newNode.next = top;
+        top = newNode;
+    }
+
+    public int pop(){
+        Node temp = top;
+        top = top.next;
+        temp.next = null;
+
+        return temp.val;
+    }
+
+    public int peek(){
+        return top.val;
+    }
 
 }
