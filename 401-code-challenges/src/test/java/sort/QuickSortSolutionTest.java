57bd78b5921e5b98d8356e08e09e49be673d14a1
Yuan Gao <example@email.com>
2019-07-02 20:48:58 -0700

    finish quicksort test

--- 401-code-challenges/src/test/java/sort/QuickSortSolutionTest.java
+++ 401-code-challenges/src/test/java/sort/QuickSortSolutionTest.java
@@ -6,15 +6,31 @@ import static org.junit.Assert.*;
 
 public class QuickSortSolutionTest {
 
+    //test if arr is empty
     @Test
-    public void swap() {
+    public void test_empty() {
+        QuickSortSolution test = new QuickSortSolution();
+        int arr[] = {};
+        test.quickSort(arr,0,0);
     }
 
+    //test if array has only one element
     @Test
-    public void partition() {
+    public void test_one_array() {
+        QuickSortSolution test = new QuickSortSolution();
+        int arr[] = {4};
+        test.quickSort(arr,0,0);
+        int expect[] = {4};
+        assertArrayEquals(expect,arr);
     }
 
+    //test normal array
     @Test
-    public void quickSort() {
+    public void test_quickSort() {
+        QuickSortSolution test = new QuickSortSolution();
+        int arr[] = {4,3,6,1};
+        test.quickSort(arr,0,3);
+        int expect[] = {1,3,4,6};
+        assertArrayEquals(expect,arr);
     }
 }
