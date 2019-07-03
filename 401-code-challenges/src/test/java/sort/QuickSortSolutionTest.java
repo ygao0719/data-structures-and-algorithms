2ed00a22cd2c68caf6f5b558218211129a049bfd
Yuan Gao <example@email.com>
2019-07-02 20:32:39 -0700

    finish class quicksort

---
+++ 401-code-challenges/src/test/java/sort/QuickSortSolutionTest.java
@@ -0,0 +1,20 @@
+package sort;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class QuickSortSolutionTest {
+
+    @Test
+    public void swap() {
+    }
+
+    @Test
+    public void partition() {
+    }
+
+    @Test
+    public void quickSort() {
+    }
+}
